3811e0c5fa9ed6f0d759807f0f45c3c306babf91
jlstro <jstrozyk@gmail.com>
2026-04-10 18:03:23 +0200

    pull logo from cdn

--- src/login/DarcLogo.tsx
+++ src/login/DarcLogo.tsx
@@ -1,4 +1,4 @@
-const DEFAULT_LOGO_URL = "/darc-logo.svg";
+const DEFAULT_LOGO_URL = "https://cdn.investigativedata.org/style/logos/darc/svg/DARC-Logo-RGB-Square-Triangle-Neg.svg";
 
 export default function DarcLogo({ size = 36, src = DEFAULT_LOGO_URL }: { size?: number; src?: string }) {
     return (
